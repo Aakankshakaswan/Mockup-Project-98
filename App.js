53a554e2bfdb0e7e7f5fd8b7f22378c9ae1d632f
Aakankshakaswan <141355208+Aakankshakaswan@users.noreply.github.com>
2023-09-18 17:57:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./Screens/LoginScreen";
+import RegisterScreen from "./Screens/Register";
+import HomeScreen from "./Screens/HomeScreen";
+
+
+const Stack = createStackNavigator();
+
+const StackNav = () => {
+  return(
+  <Stack.Navigator initialRouteName="Login"  screenOptions={{
+    headerShown: false,
+    gestureEnabled: false
+  }}>
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+    <Stack.Screen name="HomrScreen" component={HomeScreen} />
+  </Stack.Navigator>)
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StackNav/>
+    </NavigationContainer>)
+
+}
---
+++ Screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from  "react";
+import {Text, View, Image} from 'react-native';
+
+export default class HomeScreen extends  React.Component {
+    render() {
+        return(
+            <View>
+                <Text>
+                    HomeScreen
+                </Text>
+            </View>
+        )
+    }
+}
+
+
+
+
+
